b56e6b1fd9665ee9967082194ad03791ff0a3009
David St-Hilaire <sthilaire.david@gmail.com>
2020-08-09 23:23:21 -0400

    started work on interactive result buffer

--- codesearch.el
+++ codesearch.el
@@ -28,37 +28,86 @@
 
 (defun codesearch-search (query data &optional is-case-sensitive?)
   (defun split-query (query)
-    (let ((l (length query))
-          (res nil))
-      (if (< l 3)
-          (list (cons query 0))
-        (dotimes (index (- l 2) res)
-          (setq res (cons (cons (substring query index (+ index 3))
-                                index)
-                          res))))))
+    (mapcar (lambda (sub-query) (let ((l (length sub-query))
+                                      (res nil))
+                                  (if (< l 3)
+                                      (list (cons sub-query 0))
+                                    (dotimes (index (- l 2) res)
+                                      (setq res (cons (cons (substring sub-query index (+ index 3))
+                                                            index)
+                                                      res))))))
+            (split-string query " *| *" t " *")))
 
   (let* ((tri-queries (split-query query))
          (data-trigrams (elt data (if is-case-sensitive? 0 1)))
-         (data-indices (elt data 2))
-         (queries-indices (mapcar (lambda (tri-q) (seq-position data-trigrams (car tri-q))) tri-queries)))
-    (if (seq-some 'not queries-indices)
-        nil
-      (let ((queries-res (seq-mapn (lambda (tri-q index) (let ((offset (cdr tri-q)))
-                                                           (mapcar (lambda (x) (- x offset))
-                                                                   (elt data-indices index))))
-                                   tri-queries
-                                   queries-indices)))
-        (if (= (length queries-res) 1)
-            queries-res
-          (let* ((sorted-queries-res (sort queries-res (lambda (x y) (< (length x) (length y)))))
-                 (smallest-q-res (car sorted-queries-res)))
-            (seq-reduce (lambda (acc smallest-q-index)
-                          (if (seq-every-p (lambda (q-res) (seq-find (lambda (index) (= smallest-q-index index))
-                                                                     q-res))
-                                           (cdr sorted-queries-res))
-                              (cons smallest-q-index acc)
-                            acc))
-                        smallest-q-res
-                        nil)))))))
+         (data-indices (elt data 2)))
+    (mapcar (lambda (tri-query)
+              (let ((queries-indices (mapcar (lambda (tri-q) (seq-position data-trigrams (car tri-q))) tri-query)))
+                (if (seq-some 'not queries-indices)
+                    nil
+                  (let ((queries-res (seq-mapn (lambda (tri-q index) (let ((offset (cdr tri-q)))
+                                                                       (mapcar (lambda (x) (- x offset))
+                                                                               (elt data-indices index))))
+                                               tri-query
+                                               queries-indices)))
+                    (if (= (length queries-res) 1)
+                        queries-res
+                      (let* ((sorted-queries-res (sort queries-res (lambda (x y) (< (length x) (length y)))))
+                             (smallest-q-res (car sorted-queries-res)))
+                        (seq-reduce (lambda (acc smallest-q-index)
+                                      (if (seq-every-p (lambda (q-res) (seq-find (lambda (index) (= smallest-q-index index))
+                                                                                 q-res))
+                                                       (cdr sorted-queries-res))
+                                          (cons smallest-q-index acc)
+                                        acc))
+                                    smallest-q-res
+                                    nil)))))))
+            tri-queries)))
 
-(pp (codesearch-search "string" (codesearch-get-trigrams (buffer-file-name (current-buffer))) t))
+(pp (codesearch-search "  string|   seq" (codesearch-get-trigrams (buffer-file-name (current-buffer))) t))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; interactive search and results presentation
+
+(defun codesearch-current-word-or-region ()
+  "returns string around the point, if no region is being defined, else will return the region content."
+  (if (use-region-p)
+	  (buffer-substring (region-beginning) (region-end))
+	(current-word)))
+
+(define-derived-mode codesearch-results-mode tabulated-list-mode "codesearch-results mode"
+  "Major mode browsing codesearch query results"
+  (setq tabulated-list-format (vector (list "result"  100 t)
+                                      ;(list "line"    100 t)
+                                      ))
+  (setq tabulated-list-entries
+        (mapcar (lambda (result)
+                  (list result (vector (format "%d" result))))
+                codesearch-query-result))
+  (setq tabulated-list-sort-key (cons "result" nil))
+  (tabulated-list-init-header)
+  (use-local-map (let ((map (make-sparse-keymap)))
+                   (set-keymap-parent map tabulated-list-mode-map)
+                   ;(define-key map (kbd "s") 'ide-browser-change-sort-key)
+                   ;(define-key map (kbd "<return>") 'ide-browser-find-file)
+                   map)))
+
+(defvar codesearch-global-data nil)
+
+(defun codesearch-genindex (file)
+  (interactive "f")
+  (setq codesearch-global-data (codesearch-get-trigrams file)))
+
+(defun codesearch (query)
+  "Browse results of codesearch query"
+  (interactive (let ((word (codesearch-current-word-or-region)))
+                 (list (read-string (concat "Codesearch exp (default: \"" word "\"): ")
+                                    nil 'codesearch-history))))
+
+  ;; todo: check that codesearch-global-data is valid
+  
+  (with-current-buffer (get-buffer-create "*codesearch-results*")
+    (let ((codesearch-query-result (apply 'append (codesearch-search query codesearch-global-data))))
+      (codesearch-results-mode))
+    (tabulated-list-print)
+    (switch-to-buffer (current-buffer))))
